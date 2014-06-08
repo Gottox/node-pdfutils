c56777c7924dba379af6cc7bdcce889fc52f9450
Sam Rijs <srijs@airpost.net>
2014-06-08 13:57:04 -0400

    page: implement textAttributes property

--- src/page.h
+++ src/page.h
@@ -22,6 +22,7 @@ class Page : public node::ObjectWrap {
 		v8::Persistent<v8::Array> links;
 
 		static v8::Handle<v8::Value> GetLinks(v8::Local<v8::String> property, const v8::AccessorInfo &info);
+		static v8::Handle<v8::Value> GetTextAttributes(v8::Local<v8::String> property, const v8::AccessorInfo &info);
 
 		static v8::Handle<v8::Value> ConvertTo(const v8::Arguments& args);
 
