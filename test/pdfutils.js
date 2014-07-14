e4fbc21367cb9f4a5e3c49531398acd4b6ac7906
Enno Boland <g@s01.de>
2014-07-14 12:02:52 +0200

    add tests for errors while loading asynchronously

--- test/pdfutils.js
+++ test/pdfutils.js
@@ -1,5 +1,31 @@
 var fs = require("fs");
 
+function testDocumentStructure(pdf) {
+	expect(pdf).to.be.an(Object);
+	expect(pdf.length).to.be.greaterThan(0);
+	expect(pdf).to.have.property('author');
+	expect(pdf).to.have.property('creation_date');
+	expect(pdf).to.have.property('keywords');
+	expect(pdf).to.have.property('linearized');
+	expect(pdf).to.have.property('metadata');
+	expect(pdf).to.have.property('modification_date');
+	expect(pdf).to.have.property('pageLayout');
+	expect(pdf).to.have.property('pageMode');
+	expect(pdf).to.have.property('producer');
+	expect(pdf).to.have.property('subject');
+	expect(pdf).to.have.property('title');
+	expect(pdf.permissions).to.only.have.keys(
+		'print',
+		'modify',
+		'copy',
+		'addNotes',
+		'fillForm',
+		'extractContents',
+		'assemble',
+		'printHighResolution'
+	);
+}
+
 describe('pdfutils', function() {
 	it("can be initialized", function() {
 		pdfutils();
@@ -9,50 +35,36 @@ describe('pdfutils', function() {
 
 	it("can load PDF File synchronously", function() {
 		var pdf = pdfutils(__dirname + "/pdfs/testfile1.pdf");
-		expect(pdf).to.be.an(Object);
-		expect(pdf.length).to.be.greaterThan(0);
-		expect(pdf).to.have.property('author');
-		expect(pdf).to.have.property('creation_date');
-		expect(pdf).to.have.property('keywords');
-		expect(pdf).to.have.property('linearized');
-		expect(pdf).to.have.property('metadata');
-		expect(pdf).to.have.property('modification_date');
-		expect(pdf).to.have.property('pageLayout');
-		expect(pdf).to.have.property('pageMode');
-		expect(pdf).to.have.property('producer');
-		expect(pdf).to.have.property('subject');
-		expect(pdf).to.have.property('title');
-		expect(pdf.permissions).to.only.have.keys(
-				'print',
-				'modify',
-				'copy',
-				'addNotes',
-				'fillForm',
-				'extractContents',
-				'assemble',
-				'printHighResolution'
-		);
+		testDocumentStructure(pdf);
 	});
 
 	it("can load PDF Data synchronously", function() {
 		var pdf = pdfutils(fs.readFileSync(__dirname + "/pdfs/testfile1.pdf"));
-		expect(pdf).to.be.an(Object);
+		testDocumentStructure(pdf);
 	})
 
 	it("can load PDF File asynchronously", function(done) {
 		var pdf = pdfutils(__dirname + "/pdfs/testfile1.pdf", function(err, pdf) {
-			expect(pdf).to.be.an(Object)
+			testDocumentStructure(pdf);
 			done();
 		});
 		expect(pdf).to.be(undefined);
 	});
 
-	it("should throw on nonexistent file", function() {
+	it("should throw on nonexistent file on synchronous load", function() {
 		try {
 			var pdf = pdfutils(__dirname + "/pdfs/UNEXISTENT.pdf");
 			expect().fail("does not throw")
 		} catch(e) {
 		}
-	})
+	});
+
+	it("should callback error on nonexistent file on asynchronous load", function(done) {
+		var pdf = pdfutils(__dirname + "/pdfs/UNEXISTENT.pdf", function(err, pdf) {
+			expect(err).to.be.an(Error);
+			expect(pdf).to.be(null);
+			done();
+		});
+	});
 });
 
