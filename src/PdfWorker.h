39a05c7cf94d199855a8553d354b439d15b487a7
Enno Boland <g@s01.de>
2014-07-15 17:39:06 +0200

    adding intermediate callbacks to PdfWorker

--- src/PdfWorker.h
+++ src/PdfWorker.h
@@ -9,21 +9,64 @@
 #define PDFWORKER_H
 
 #include <nan.h>
+#include <uv.h>
+#include <list>
 #include "PdfController.h"
+
 
 template <class T>
 class PdfWorker : public NanAsyncWorker {
 private:
 	T *_controller;
+	uv_async_t async;
+	uv_mutex_t mutex;
+	std::list<void *> intermediate;
+
+
+	static void handleIntermediate(uv_async_t *handle, int status /*UNUSED*/) {
+		PdfWorker *self = (PdfWorker *)handle->data;
+		void *data;
+
+		while(true) {
+			uv_mutex_lock(&self->mutex);
+			if(self->intermediate.empty()) {
+				uv_mutex_unlock(&self->mutex);
+				break;
+			} else {
+				data = self->intermediate.front();
+				self->intermediate.pop_front();
+				uv_mutex_unlock(&self->mutex);
+			}
+			self->HandleIntermediate(data);
+		}
+	}
 
 public:
 	PdfWorker(T *controller, NanCallback *callback) : NanAsyncWorker(callback) {
 		_controller = controller;
+
+		uv_loop_t *loop = uv_default_loop();
+		uv_async_init(loop, &async, handleIntermediate);
+		async.data = this;
+		uv_mutex_init(&mutex);
+	}
+
+	virtual ~PdfWorker() {
+		uv_unref((uv_handle_t*)&this->async);
+		uv_mutex_destroy(&this->mutex);
 	}
 
 	T *controller() {
 		return _controller;
 	}
+
+	void CallIntermediate(void *data) {
+		uv_mutex_lock(this->mutex);
+		intermediate.push_back(data);
+		uv_mutex_unlock(this->mutex);
+	}
+
+	virtual void HandleIntermediate(void *data) {}
 };
 
 #endif /* !PDFWORKER_H */
