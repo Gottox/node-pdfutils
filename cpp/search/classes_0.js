bfc453899f696a2b1155b1685c6bc7768a207b85
Enno Boland <g@s01.de>
2015-04-17 02:22:41 +0200

    rebuild docs based on 6af3724c69289fde7fc63626c2b8ab2b29e00bfd

--- cpp/search/classes_0.js
+++ cpp/search/classes_0.js
@@ -5,13 +5,15 @@ var searchData=
   ['pdfdocumentcontroller',['PdfDocumentController',['../classPdfDocumentController.html',1,'']]],
   ['pdfengine',['PdfEngine',['../classPdfEngine.html',1,'']]],
   ['pdfenginefactory',['PdfEngineFactory',['../classPdfEngineFactory.html',1,'']]],
+  ['pdfexportpageworker',['PdfExportPageWorker',['../classPdfExportPageWorker.html',1,'']]],
   ['pdfopendataworker',['PdfOpenDataWorker',['../classPdfOpenDataWorker.html',1,'']]],
   ['pdfopenfileworker',['PdfOpenFileWorker',['../classPdfOpenFileWorker.html',1,'']]],
   ['pdfopenworker',['PdfOpenWorker',['../classPdfOpenWorker.html',1,'']]],
   ['pdfpage',['PdfPage',['../classPdfPage.html',1,'']]],
   ['pdfpagecontroller',['PdfPageController',['../classPdfPageController.html',1,'']]],
+  ['pdfsaveworker',['PdfSaveWorker',['../classPdfSaveWorker.html',1,'']]],
   ['pdfworker',['PdfWorker',['../classPdfWorker.html',1,'']]],
   ['pdfworker_3c_20pdfdocumentcontroller_20_3e',['PdfWorker&lt; PdfDocumentController &gt;',['../classPdfWorker.html',1,'']]],
-  ['pdfwriter',['PdfWriter',['../classPdfWriter.html',1,'']]],
+  ['pdfworker_3c_20pdfpagecontroller_20_3e',['PdfWorker&lt; PdfPageController &gt;',['../classPdfWorker.html',1,'']]],
   ['popplerengine',['PopplerEngine',['../classPopplerEngine.html',1,'']]]
 ];
---
+++ cpp/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "acdefhiklmnpstw",
+  1: "p",
+  2: "acdefhiklmnpstw"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Functions"
+};
+
